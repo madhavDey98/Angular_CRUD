821766acede08fc85f7f6f21cf49105934829a5b
Maddydey233 <madhavendradey69@gmail.com>
2023-07-23 21:37:14 +0530

    More edits

--- Angular_CRUD/src/app/request-mapper.ts
+++ Angular_CRUD/src/app/request-mapper.ts
@@ -18,5 +18,7 @@ export class RequestMapper {
 
   public static readonly BASE_API_URL: string = 'https://reqres.in/api/';
   public static readonly API_LOGIN: string = 'login';
+  public static readonly API_CREATE_USER: string = 'users';
   public static readonly API_USER_LIST: string = 'users?page=2';
+  public static readonly API_EMP_LIST: string = 'employees';
 }
